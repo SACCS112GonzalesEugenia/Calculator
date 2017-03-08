68c61a38849e90b5ba7c90949b4b9eace4c94e84
Eugenia Gonzales <e.flores.gonzales@gmail.com>
2017-03-07 21:18:26 -0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Calculator{
+    @SuppressWarnings("resource")
+	public static void main(String[] args) {       
+
+Scanner input = new Scanner(System.in);
+        int number1;       
+
+        int number2;       
+
+        int sum;       
+
+       int difference;       
+
+       int product;       
+
+       int quotient;               
+
+       System.out.print("Enter first number: ");     
+       number1 = input.nextInt();
+       
+       System.out.print("Enter second number: ");
+        number2 = input.nextInt();
+        
+        sum = number1 + number2;               
+        difference = number1 - number2;
+        product = number1 * number2;
+        quotient = number1 / number2;  
+        
+        System.out.printf("The sum is: %d\n", sum);       
+
+       System.out.printf("The difference is: %d\n",difference);      
+
+       System.out.printf("The product is: %d\n", product);      
+
+       System.out.printf("The quotient is: %d\n", quotient); 
+    }
+
+}
+
